d2cccaf3966f4cd2ce43a7cbe3e282e034816a3a
Joseph Smith <joseph@forgottenarts.com>
2012-05-03 21:51:27 -0700

    added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Inviter::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+ get "static_pages/home"
+ get "static_pages/help"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
