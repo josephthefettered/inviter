ca1fc99f59a33ed879156c9e0c676196cb1a5e45
Joseph Smith <joseph@forgottenarts.com>
2012-05-04 13:51:16 -0700

    6.2.4 complete, atp

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :fName
 
-  validates :fName, presence: true
-  validates :email, presence: true
+  validates :fName, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,5 +32,29 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when fName is too long" do
+  	before { @user.fName = "a" * 51 }
+  	it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
 
 end
