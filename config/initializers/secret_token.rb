82be8e7f10fc248b5338dba8571f7f001d55c998
Joseph Smith <joseph@forgottenarts.com>
2012-05-03 21:20:48 -0700

    initial inviter video

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inviter::Application.config.secret_token = '84712956a293edfd2ceb73ea37b6620e03fc56e332af8fb24fed47cad11020ffed4f0e51932ddd5221c9f71afd4793d154c72b650402e4811022f41f14131bb3'
