7a4686196e33e3ef00e116fd9eeeaf3fe3088b32
Joseph Smith <joseph@forgottenarts.com>
2012-05-04 17:31:11 -0700

    ch 6.3.4 complete all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,15 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :fName
+  attr_accessible :email, :fName, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
 
   validates :fName, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-
-
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120504201548) do
+ActiveRecord::Schema.define(:version => 20120504222954) do
 
   create_table "users", :force => true do |t|
     t.string   "fName"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
