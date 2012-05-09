04c292777181b677eabb9ec394c41c0d3aa91a10
Joseph Smith <joseph@forgottenarts.com>
2012-05-08 23:08:09 -0700

    basic user model, secure authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505013746) do
+ActiveRecord::Schema.define(:version => 20120509055925) do
 
   create_table "users", :force => true do |t|
     t.string   "fName"
