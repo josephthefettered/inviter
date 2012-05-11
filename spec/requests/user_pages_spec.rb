be6605453d2a484c889cda035c22f428e41684d8
Joseph Smith <joseph@forgottenarts.com>
2012-05-10 22:39:39 -0700

    user signup complete

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,6 @@ describe "User pages" do
 
   subject { page }
 
-
-
   describe "profile page" do
   		let(:user) { FactoryGirl.create(:user) }
   		before { visit user_path(user) }
@@ -18,8 +16,25 @@ describe "User pages" do
 
   describe "signup page" do
     before { visit signup_path }
+    let(:submit) { "Create my account" }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Tester Sandwich"
+        fill_in "Email",        with: "email@example.com"
+        fill_in "Password",     with: "password"
+        fill_in "Confirmation", with: "password"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
